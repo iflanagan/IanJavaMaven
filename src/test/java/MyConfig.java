1b271ae18ca97ffeac39e3bb28a64ce59350a1da
Ian Flanagan <ianianf@yahoo.com>
2021-04-01 10:27:25 -0500

    removed RB token.

--- src/test/java/MyConfig.java
+++ src/test/java/MyConfig.java
@@ -2,11 +2,11 @@ import java.util.Random;
 
 public class MyConfig {
 
-    public static final String myProjectAPIToken = "17b35e5e964544dabc7e41884f1531a2";
+    public static final String myProjectAPIToken = "<token>";
     public static final String myTestEnv = Utils.changeEnv();
 
 
-    public static final String myToken = "17b35e5e964544dabc7e41884f1531a2"; //   use <post_server_item> token for your rollbar project
+    public static final String myToken = "<token>"; //   use <post_server_item> token for your rollbar project
     public static final String myENv =  Utils.changeEnv();
     public static final String myVersion = RollbarUtils.randomString(10);
     public static final String framework = "Junit5";
